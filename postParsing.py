a7f690be3d8961e289a4fcd6f417fe24be45483f
faissaloo <faissaloo@gmail.com>
2016-10-03 21:00:44 +0100

    Better post parser

--- postParsing.py
+++ postParsing.py
@@ -1,45 +1,21 @@
 #!/usr/bin/env python3
-def expect(toExpect):
-	global textCursor
-	global textContents
-	if textContents[textCursor:textCursor+len(toExpect)]==toExpect:
-		textCursor+=len(toExpect)
-		return True
-	else:
-		return False
-
-def skipWhitespace():
-	global textCursor
-	global textContents
-	while expect(" ") or expect("\t"):
-		pass
-	
-
+import re
 def parse(string):
-	global textCursor
-	global textContents
 	postDict={
 		"Name":"",
 		"Subject":"",
 		"Body":""
 	}
-	textCursor=0
-	textContents=string
-	skipWhitespace()
-	while textCursor<len(textContents):
-		if expect("Name:"):
-			skipWhitespace()
-			while textContents[textCursor]!="\n":
-				postDict["Name"]+=textContents[textCursor]
-				textCursor+=1
-		elif expect("Subject:"):
-			skipWhitespace()
-			while textContents[textCursor]!="\n":
-				postDict["Subject"]+=textContents[textCursor]
-				textCursor+=1
-		elif expect("Body:"):
-			while textCursor<len(textContents):
-				postDict["Body"]+=textContents[textCursor]
-				textCursor+=1
-		textCursor+=1
+	#Split by body first
+	bodyList=string.split("Body:",1)
+	if len(bodyList)>0:
+		postDict["Body"] = bodyList[1]
+	
+	nameList=re.findall("(?i)Name\:[\W]*(.*)\n",bodyList[0])
+	if len(nameList)>0:
+		postDict["Name"] = nameList[0]
+	
+	subList=re.findall("(?i)Subject\:[\W]*(.*)\n",bodyList[0])
+	if len(subList)>0:
+		postDict["Subject"] = subList[0]
 	return postDict
